fee19b4054d2ad67fdc6dff9fc1aa8a74e16915b
Eric Jin <jxin48598@gmail.com>
2019-08-01 18:01:24 +0800

    eat dinner

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -2,7 +2,6 @@ $(function () {
     // 匹配页面的<swiper></swiper>标签
     let swiperArr = $("swiper");
     swiperArr.each((index, swiper) => {
-        // 获取swiper的参数
         let swiperDom = $(`
             <div class="rel-ebb-swiper">
                 <div class="prev move">
@@ -14,6 +13,12 @@ $(function () {
                 <div class="point-list"></div>
             </div>
         `);
+        // 获取swiper的参数
+        let pointPanel = $(swiper).attr("point-panel");
+        let autoplay = $(swiper).attr("autoplay");
+        let pointColor = $(swiper).attr('point-color');
+        let pointActiveColor = $(swiper).attr('point-active-color');
+        let pointPanelColor = $(swiper).attr('point-panel-color');
 
         // 获取<swiper-item></swiper-item>标签
         let swiperItemArr = $(swiper).children();
@@ -36,31 +41,53 @@ $(function () {
         });
         // 添加到swiperDom中
         swiperDom.append(swiperItemStr);
-        swiperDom.children(".point-list").append(pointStr);
+        // 是否添加下面的point panel
+        if (pointPanel !== undefined) {
+            swiperDom.children(".point-list").append(pointStr);
+        } else {
+            swiperDom.children(".point-list").remove();
+        }
+        // point panel的颜色
+        if (pointPanelColor) {
+            swiperDom.find(".point-list").css("background-color", pointPanelColor);
+        }
+        // 底部小圆点的颜色
+        if (pointColor) {
+            swiperDom.find(".point").css("background-color", pointColor);
+            // 激活的颜色
+            if (pointActiveColor) {
+                swiperDom.find(".point.active").css("background-color", pointActiveColor);
+            } else {
+                swiperDom.find(".point.active").css("background-color", "#f00");
+            }
+        }
         // 添加到页面
         $(swiper).replaceWith(swiperDom);
         // 启动banner
-        move(swiperDom);
+        move(swiperDom, autoplay, pointActiveColor, pointColor);
     });
 });
 
 /**
  * 点击切换banner图
  * @param swiperDom
+ * @param autoplay
+ * @param pointActiveColor
+ * @param pointColor
  */
-function move(swiperDom) {
+function move(swiperDom, autoplay, pointActiveColor, pointColor) {
     let index = 0;
     let timer = null;
     // 设置prev点击事件
     $(swiperDom).children(".prev").click(() => {
         index = index <= 0 ? $(swiperDom).children(".rel-ebb-swiper-item").length - 1 : --index;
-        changeItem(swiperDom, index);
+        changeItem(swiperDom, index, pointActiveColor, pointColor);
     });
 
     // 设置next点击事件
     $(swiperDom).children(".next").click(() => {
         index = index >= $(swiperDom).children(".rel-ebb-swiper-item").length - 1 ? 0 : ++index;
-        changeItem(swiperDom, index);
+        changeItem(swiperDom, index, pointActiveColor, pointColor);
     });
 
     // 设置point-list点击事件
@@ -69,40 +96,55 @@ function move(swiperDom) {
         if (currentDom.localName === 'span') {
             // 获取index
             index = parseInt($(currentDom).data('index'));
-            changeItem(swiperDom, index);
+            changeItem(swiperDom, index, pointActiveColor, pointColor);
         }
     });
 
-    // 设置动画
-    timer = setInterval(() => {
-        index = index >= $(swiperDom).children(".rel-ebb-swiper-item").length - 1 ? 0 : ++index;
-        changeItem(swiperDom, index);
-    }, 2000);
-    $(swiperDom).mouseover(() => {
-        // 鼠标移入，动画消失
-        clearInterval(timer);
-    });
-    $(swiperDom).mouseout(() => {
-        // 鼠标移出，动画恢复
+    if (autoplay !== undefined) {
+        // 设置动画
         timer = setInterval(() => {
             index = index >= $(swiperDom).children(".rel-ebb-swiper-item").length - 1 ? 0 : ++index;
-            changeItem(swiperDom, index);
+            changeItem(swiperDom, index, pointActiveColor, pointColor);
         }, 2000);
-    });
+        $(swiperDom).mouseover(() => {
+            // 鼠标移入，动画消失
+            clearInterval(timer);
+        });
+        $(swiperDom).mouseout(() => {
+            // 鼠标移出，动画恢复
+            timer = setInterval(() => {
+                index = index >= $(swiperDom).children(".rel-ebb-swiper-item").length - 1 ? 0 : ++index;
+                changeItem(swiperDom, index, pointActiveColor, pointColor);
+            }, 2000);
+        });
+    }
 }
 
 /**
  * 切换样式
  * @param swiperDom
  * @param index
+ * @param pointActiveColor
+ * @param pointColor
  */
-function changeItem(swiperDom, index) {
+function changeItem(swiperDom, index, pointActiveColor, pointColor) {
     // 显示对应的banner
     $(swiperDom).children(".rel-ebb-swiper-item").removeClass('active');
     $($(swiperDom).children(".rel-ebb-swiper-item").get(index)).addClass('active');
     // 替换底部按钮
     $(swiperDom).find(".point").removeClass('active');
     $($(swiperDom).find(".point").get(index)).addClass('active');
+
+    // 底部小圆点的颜色
+    if (pointColor) {
+        swiperDom.find(".point").css("background-color", pointColor);
+        // 激活的颜色
+        if (pointActiveColor) {
+            swiperDom.find(".point.active").css("background-color", pointActiveColor);
+        } else {
+            swiperDom.find(".point.active").css("background-color", "#f00");
+        }
+    }
 }
 
 
